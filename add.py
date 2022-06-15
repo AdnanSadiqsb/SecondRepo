bc3f1f3cf6ce56a0522ae2f6847b1d02082c601b
adnansadiqsb <adnansadiqxyz@gmail.com>
2022-06-15 20:56:19 +0500

    after uploding on th git hub

--- add.py
+++ add.py
@@ -2,3 +2,5 @@ print(3+5)
 print("this is add function after commit")
 print("hlo")
 print("multiply function is added in adnan/multiply_funcion function")
+
+print("this is added after upload on git")
